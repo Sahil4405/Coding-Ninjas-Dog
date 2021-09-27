9eb5bb6acfad87bb10fcf47b7b6cf309b007ca79
lovnish044 <lovnishgarg044@gmail.com>
2021-09-27 11:51:08 +0530

    ass

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const Dog = (props) => {
+  return React.createElement("div", {}, [
+      React.createElement('h3', {}, props.name),
+      React.createElement('h3', {}, props.weight),
+      React.createElement('h3', {}, props.height),
+
+  ])
+}
+
+const App = () => {
+  var arr = [1, 2, 3, 4];
+  return React.createElement(
+      "div",  
+      {id:'website-title'},   
+      [
+          React.createElement(
+              "h3",
+              {},
+              "Dog Website"
+          ),
+          ...[arr].map(item => React.createElement("h1", {}, item)),
+          React.createElement(Dog, {
+              name: "Bull Terrier",
+              weight: "50.70 Kg",
+              breed: "Terrier"
+          },),
+          React.createElement(Dog, {
+              name: "PitBull",
+              weight: "50.70 Kg",
+              breed: "Bull"
+          },),
+          React.createElement(Dog, {
+              name: "Puppy",
+              weight: "5.70 Kg",
+              breed: "husky"
+          },)
+
+      ], 
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  React.createElement(  
+      App
+      ),
+  document.getElementById('root') 
+  );
